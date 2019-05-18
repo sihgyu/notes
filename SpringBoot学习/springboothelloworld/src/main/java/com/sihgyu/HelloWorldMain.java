cef199a3b57eea9666a9533b6063bab33dd15cd6
sihgyu <1142570567@qq.com>
2019-05-18 13:57:43 +0800

    Springboot

---
+++ SpringBoot学习/springboothelloworld/src/main/java/com/sihgyu/HelloWorldMain.java
@@ -0,0 +1,12 @@
+package com.sihgyu;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMain {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMain.class,args);
+    }
+}
